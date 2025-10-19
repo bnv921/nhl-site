74a5fe8d4d3aa819607af38676f923642a2ef312
root <root@1324276.xorek.cloud>
2025-10-19 18:35:24 +0300

    Remove folder: имя_папки

---
+++ src/client/.next/static/chunks/app/layout-f35758f0499d3f50.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{173:function(e,n,t){Promise.resolve().then(t.t.bind(t,3950,23)),Promise.resolve().then(t.t.bind(t,6340,23)),Promise.resolve().then(t.t.bind(t,5646,23))},5646:function(){}},function(e){e.O(0,[490,300,293,528,744],function(){return e(e.s=173)}),_N_E=e.O()}]);
